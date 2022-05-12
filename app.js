49aa36e21613da70cb2e1106827a5c74c4c32d2a
gaetansbaffi <gaetansbaffi@live.fr>
2022-05-12 15:12:25 +0200

    Added basic UI and interactiveness each click on a box mark and change player turn

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log("hello");
+const gameBoard = (() => {
+  let board = new Array(9);
+  let container = document.querySelector(".container");
+
+  const addSquares = (board) => {
+    container.innerHTML = "";
+    for (let [index, value] of board.entries()) {
+      let box = document.createElement("div");
+      box.setAttribute("id", `${index}`);
+      box.className = "box";
+      container.appendChild(box);
+    }
+  };
+  addSquares(board);
+  return { board };
+})();
+
+const Player = (name, mark, score) => {
+  return { name, mark, score };
+};
+
+const game = (() => {
+  const player1 = Player("bill", "x", 0);
+  const player2 = Player("kim", "o", 0);
+  const box = document.querySelectorAll(".box");
+  let playerTurn = true;
+
+  box.forEach((element) => {
+    element.addEventListener("click", (e) => addMark(e));
+  });
+
+  const addMark = (e) => {
+    let player;
+
+    const changePlayer = () => {
+      if (playerTurn) {
+        player = player1;
+      } else {
+        player = player2;
+      }
+      return (playerTurn = !playerTurn);
+    };
+    changePlayer();
+
+    return (e.target.textContent = player.mark);
+  };
+})();
