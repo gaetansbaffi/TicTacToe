54afa0c8d2d10401d0bb8e2afcad6d2e5c388259
gaetansbaffi <gaetansbaffi@live.fr>
2022-05-17 13:48:42 +0200

    Finished project functionnalities could add basic IA and better UI...

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".reset");
+const submitBtn = document.querySelector(".submitBtn");
+const scores = document.querySelector(".scores");
 const score1 = document.querySelector(".score1");
 const score2 = document.querySelector(".score2");
+const form = document.querySelector("form");
+let player1;
+let player2;
+let winner;
 let gameState = true;
 
-const Player = (name, mark, score) => {
+const Player = (name, mark, score = 0) => {
   return { name, mark, score };
 };
 // set players
+submitBtn.addEventListener("click", (e) => setPlayers(e));
 
-const player1 = Player("bill", "x", 0);
-const player2 = Player("kim", "o", 0);
+const setPlayers = (data) => {
+  const player1Name = document.querySelector(".player1Name");
+  const player2Name = document.querySelector(".player2Name");
+  data.preventDefault();
+  let formData = new FormData(form);
+  let players = {};
+  for (const data of formData) {
+    players[data[0]] = data[1];
+  }
+  player1 = Player(players.player1, players.player1Mark);
+  player2 = Player(players.player2, players.player2Mark);
+  if (players.player1Mark === players.player2Mark) {
+    return alert("both players can't have the same Mark!");
+  }
+  player1Name.textContent = player1.name + ": ";
+  player2Name.textContent = player2.name + ": ";
+  scores.classList.toggle("hidden");
+  form.classList.toggle("hidden");
+  container.classList.toggle("hidden");
+  Game(player1, player2);
+};
 
-//set game
+//!SET GAME
 const Game = (player1, player2) => {
   //variables
   const box = document.querySelectorAll(".box");
@@ -53,14 +79,8 @@ const Game = (player1, player2) => {
           checkWin(tempArr2);
         }
       }
-      if (!board.includes(undefined)) {
-        gameState = false;
-        alert("it's a tie!");
-        resetBtn.classList.toggle("hidden");
-        resetBtn.addEventListener("click", (e) => restart(gameBoard.board));
-      }
     };
-
+    //WIN LOGIC
     const checkWin = (arr) => {
       const rows = [
         [0, 1, 2],
@@ -77,14 +97,21 @@ const Game = (player1, player2) => {
         [2, 4, 6],
       ];
       const winCons = [...rows, ...cols, ...cross];
-
-      for (const winCon of winCons) {
-        if (checkWin(winCon, arr)) {
-          gameState = false;
-          player.score++;
-          console.log("hi");
-          updateScores();
-          restart();
+      if (!gameBoard.board.includes(undefined)) {
+        gameState = false;
+        winner = null;
+        alert("it's a tie!");
+        updateScores();
+        restart();
+      } else {
+        for (const winCon of winCons) {
+          if (checkWin(winCon, arr)) {
+            gameState = false;
+            winner = player;
+            player.score++;
+            updateScores();
+            restart();
+          }
         }
       }
 
@@ -99,7 +126,7 @@ const Game = (player1, player2) => {
       score1.textContent = player1.score;
       score2.textContent = player2.score;
     };
-
+    //RESTART FUNCTIONS
     const restart = () => {
       const playAgain = () => {
         resetBtn.classList.toggle("hidden");
@@ -108,7 +135,6 @@ const Game = (player1, player2) => {
 
       const resetGame = () => {
         container.innerHTML = "";
-
         gameBoard.board = new Array(9);
         gameBoard.addSquares(gameBoard.board);
         resetBtn.className += " hidden";
@@ -118,17 +144,21 @@ const Game = (player1, player2) => {
       };
 
       if (player1.score >= 3 || player2.score >= 3) {
-        //game is over
         playAgain();
-        alert(`${player.name} wins! Click reset to start a new game!`);
+        const winner = document.createElement("h2");
+        winner.className = "winner";
+        winner.innerHTML = `${player.name} wins! Click reset to start a new game!`;
+        container.append(winner);
         player1.score = player2.score = 0;
 
         return (gameState = false);
       } else {
-        const winner = document.createElement("h2");
-        winner.className = "winner";
-        winner.innerHTML = `${player.name} wins!`;
-        container.prepend(winner);
+        if (winner) {
+          const winner = document.createElement("h2");
+          winner.className = "winner";
+          winner.innerHTML = `${player.name} wins!`;
+          container.append(winner);
+        }
         return playAgain();
       }
     };
@@ -150,7 +180,7 @@ const Game = (player1, player2) => {
   };
 };
 
-// set gameBoard
+// SET GAMEBOARD FOR FIRST GAME
 const gameBoard = (() => {
   let board = new Array(9);
   const addSquares = (board) => {
@@ -165,6 +195,6 @@ const gameBoard = (() => {
     }
   };
   addSquares(board);
-  Game(player1, player2);
+
   return { board, addSquares };
 })();
